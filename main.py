797751cba74933799737a8a28670e9d4118c2e36
Developer-Ganesha <ganeshsavner320@gmail.com>
2025-03-12 14:53:27 +0530

    added the port

--- main.py
+++ main.py
@@ -79,6 +79,6 @@ def logout(token: str = Depends(oauth2_scheme)):
     blacklisted_tokens.add(token)
     return {"message": "Logged out successfully"}
 
-if _name_ == "__main__":
+if __name__=="__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
+    uvicorn.run(app,host="0.0.0.0",port=8000,log_level="debug")
